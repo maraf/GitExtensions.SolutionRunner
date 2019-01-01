d83763dc422a463784d78da38a10e5d7d4313f71
Marek Fišera <mara@neptuo.com>
2019-01-01 21:59:55 +0100

    #1 - Empty Plugin.

---
+++ src/GitExtensions.SolutionRunner/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using GitExtensions.SolutionRunner.Properties;
+using GitUIPluginInterfaces;
+using ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitExtensions.SolutionRunner
+{
+    [Export(typeof(IGitPlugin))]
+    public class Plugin : GitPluginBase
+    {
+        public Plugin()
+        {
+            Name = "SolutionRunner";
+            Description = "Solution Runner";
+            Icon = Resources.Icon;
+        }
+
+        public override bool Execute(GitUIEventArgs e)
+            => true;
+    }
+}
