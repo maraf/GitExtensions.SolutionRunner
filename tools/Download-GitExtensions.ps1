b52dfea6e2851b460f8cfe357f2e68108db7d0fd
Marek Fišera <mara@neptuo.com>
2019-04-09 18:46:01 +0200

    #6 - Integration of changes for debugging from EmptyPlugin.

--- tools/Download-GitExtensions.ps1
+++ tools/Download-GitExtensions.ps1
@@ -2,8 +2,8 @@ Set-Location $PSScriptRoot
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ExtractRootPath = '../references'
-$AssetToDownloadName = 'GitExtensions-Portable-3.00.00.4433.zip';
-$AssetToDownloadUrl = 'https://github.com/gitextensions/gitextensions/releases/download/v3.00.00/GitExtensions-Portable-3.00.00.4433.zip';
+$AssetToDownloadName = 'GitExtensions-Portable-3.0.2.5232.zip';
+$AssetToDownloadUrl = 'https://github.com/gitextensions/gitextensions/releases/download/v3.0.2/GitExtensions-Portable-3.0.2.5232.zip';
 
 if (!($null -eq $AssetToDownloadUrl))
 {
