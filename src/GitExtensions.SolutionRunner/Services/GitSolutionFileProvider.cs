c2f0198a61a96748a2368191f2f7210835a08919
MTimoshinin <m.timoshinin@mail.ru>
2020-11-13 14:11:33 +0200

    fixed search performance
    excluded untracked files
    use one query for both patterns

--- src/GitExtensions.SolutionRunner/Services/GitSolutionFileProvider.cs
+++ src/GitExtensions.SolutionRunner/Services/GitSolutionFileProvider.cs
@@ -20,15 +20,9 @@ namespace GitExtensions.SolutionRunner.Services
 
         public async Task<IReadOnlyCollection<string>> GetListAsync(bool isTopLevelSearchOnly, bool includeWorkspaces)
         {
-            IProcess process = executor.Start("ls-files -coz -- *.sln", redirectOutput: true, outputEncoding: Encoding.Default);
+            string command = "ls-files -cz *.sln" + (includeWorkspaces ? " *.code-workspace" : "");
+            IProcess process = executor.Start(command, redirectOutput: true, outputEncoding: Encoding.Default);
             string output = await process.StandardOutput.ReadToEndAsync();
-
-            if (includeWorkspaces)
-            {
-                process = this.executor.Start("ls-files -coz -- *.code-workspace", redirectOutput: true, outputEncoding: Encoding.Default);
-                output += '\0';
-                output += await process.StandardOutput.ReadToEndAsync();
-            }
 
             var result = output.Split(new[] { '\0' }, System.StringSplitOptions.RemoveEmptyEntries)
                 .Where(x => !isTopLevelSearchOnly || !x.Contains('/'))
