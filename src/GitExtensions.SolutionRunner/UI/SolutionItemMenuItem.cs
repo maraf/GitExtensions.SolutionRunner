55cd472fcb4a45f92d8b68b07b74ba916f4e3b5c
Marek Fišera <mara@neptuo.com>
2022-10-24 14:44:01 +0200

    Update src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs

--- src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs
+++ src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs
@@ -35,7 +35,8 @@ namespace GitExtensions.SolutionRunner.UI
                 process.StartInfo.FileName = settings.ExecutablePath; 
                 process.StartInfo.Arguments =  settings.ExecutableArguments?.Replace(PluginSettings.DefaultExecutableArguments, filePath) ?? filePath;
 
-                if(settings.ShouldRunAsAdmin)
+                if (settings.ShouldRunAsAdmin)
+
                     process.StartInfo.Verb = RunAsAdminConfiguration;
 
                 process.Start();
