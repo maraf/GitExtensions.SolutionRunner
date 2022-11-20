7f6010f76a36129557047756c1367d746b5870c7
Angie Kadnar <aka+code@outlook.at>
2022-11-20 15:52:28 +0100

    #28 - Update csproj to .net7 to fix runtime version conflict

--- src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs
+++ src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace GitExtensions.SolutionRunner.UI
