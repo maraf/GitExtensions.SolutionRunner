0305c67e6ef4dc955bc29e69ec167e5df2bd0e55
Angie Kadnar <aka+code@outlook.at>
2022-11-23 21:28:17 +0100

    #28 - Clean up all usings

--- src/GitExtensions.SolutionRunner/PluginSettings.cs
+++ src/GitExtensions.SolutionRunner/PluginSettings.cs
@@ -1,10 +1,6 @@
 ﻿using GitUIPluginInterfaces;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitExtensions.SolutionRunner
 {
--- src/GitExtensions.SolutionRunner/UI/SolutionListMenuItem.cs
+++ src/GitExtensions.SolutionRunner/UI/SolutionListMenuItem.cs
@@ -1,9 +1,6 @@
 ﻿using GitExtensions.SolutionRunner.Services;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
