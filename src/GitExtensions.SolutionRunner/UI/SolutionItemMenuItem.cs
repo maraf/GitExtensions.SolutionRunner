c1f683f7dc30b9afa51511c913c5d97abe822604
Angelika Kadnar <45826225+AngieKadnar@users.noreply.github.com>
2023-12-14 10:43:25 +0100

    Add logic to start VsCode as Admin (#38)
    * #37 Add Logic to start VsCode as Admin with GitExtensions.SolutionRunner
    
    * #37 Implement logic to find preferred executable for file extension and simplify code
    
    * #37 Use regex to extract executable application
    
    ---------
    
    Co-authored-by: Angelika Kadnar (AKD) <a.kadnar@uppersolutions.at>

--- src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs
+++ src/GitExtensions.SolutionRunner/UI/SolutionItemMenuItem.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace GitExtensions.SolutionRunner.UI
 {
     public class SolutionItemMenuItem : ToolStripMenuItem
     {
         private const string RunAsAdminConfiguration = "runas";
+        private const string ExtractExecutablePattern = @"^""([^""""]*)""";
 
         private readonly string filePath;
         private readonly PluginSettings settings;
@@ -28,10 +33,12 @@ namespace GitExtensions.SolutionRunner.UI
                 ?.Replace(PluginSettings.SolutionDirectoryToken, Path.GetDirectoryName(filePath));
 
             var process = new Process();
-            process.StartInfo.FileName = !string.IsNullOrWhiteSpace(settings.ExecutablePath)
-                ? settings.ExecutablePath
-                : filePath;
-            process.StartInfo.Arguments = arguments ?? filePath;
+
+            process.StartInfo.FileName = string.IsNullOrWhiteSpace(settings.ExecutablePath)
+                ? GetPreferredExecutablePathOrFilePath()
+                : settings.ExecutablePath;
+
+            process.StartInfo.Arguments = string.IsNullOrWhiteSpace(arguments) ? filePath : arguments;
             process.StartInfo.UseShellExecute = true;
 
             if (settings.ShouldRunAsAdmin)
@@ -40,6 +47,52 @@ namespace GitExtensions.SolutionRunner.UI
             process.Start();
 
             base.OnClick(e);
+        }
+
+        private string GetPreferredExecutablePathOrFilePath()
+        {
+            var programId = TryFindPreferredExecutableProgram();
+            var executablePath = TryFindPreferredExecutableProgramPath(programId);
+
+            return executablePath ?? filePath;
+        }
+
+        private object TryFindPreferredExecutableProgram()
+        {
+            var fileExtension = Path.GetExtension(filePath)!;
+            var extensionKey = Registry.ClassesRoot.OpenSubKey(fileExtension);
+            var programId = GetAssociatedProgramId(extensionKey);
+
+            var hasFileExtensionNotAssociatedProgramId = programId == null;
+            if (hasFileExtensionNotAssociatedProgramId)
+            {
+                extensionKey = extensionKey?.OpenSubKey("OpenWithProgids");
+                programId = extensionKey?.GetValueNames().FirstOrDefault();
+            }
+
+            extensionKey?.Close();
+
+            return programId;
+        }
+
+        private static string TryFindPreferredExecutableProgramPath(object programId)
+        {
+            if(programId == null)
+                return null;
+
+            var programKey = Registry.ClassesRoot.OpenSubKey(programId + @"\shell\open\command");
+            var runTemplate = GetAssociatedProgramId(programKey);
+
+            programKey?.Close();
+
+            var executableApplication = Regex.Match(runTemplate.ToString(), ExtractExecutablePattern).Value;
+
+            return executableApplication;
+        }
+
+        private static object GetAssociatedProgramId(RegistryKey extensionKey)
+        {
+            return extensionKey.GetValue(string.Empty);
         }
     }
 }
